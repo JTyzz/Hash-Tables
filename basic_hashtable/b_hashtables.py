0f12f70f3377095fd266b0a7322b884d7a1502c4
JTyzz <jtyzzer@gmail.com>
2019-08-06 09:40:20 -0700

    Hashtables passing 3/4 tests

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -7,50 +7,67 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.next = None
 
 
 # '''
 # Basic hash table
-# Fill this in.  All storage values should be initialized to None
+# All storage values should be initialized to None
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.count = 0
+        self.storage = [None] * capacity
 
 
-# '''
-# Fill this in.
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for character in string:
+        hash = ((hash << 5) + hash) + ord(character)
+    return hash % max
 
-
-# '''
-# Fill this in.
 
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    index = hash(key, hash_table.capacity)
 
+    current_pair = hash_table.storage[index]
 
-# '''
-# Fill this in.
+    while current_pair is not None and current_pair.key != key:
+        current_pair = current_pair.next
+
+    if current_pair is None:
+        new_pair = Pair(key, value)
+        new_pair.next = hash_table.storage[index]
+        hash_table.storage[index] = new_pair
+    else:
+        current_pair.value = value
+
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
 
+    if hash_table.storage[index] is None:
+        print("Warning: removing non-existent key" + key)
+    hash_table.storage[index] = None
 
-# '''
-# Fill this in.
 
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+
+    if hash_table.storage[index] is None:
+        print("Warning: no value found for key " + key)
+        return None
+
+    return hash_table.storage[index]
 
 
 def Testing():
